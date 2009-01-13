79d5474d0479bf3dc9e8f616b96180a3f40d6bc5
seb <seb@localhost.localdomain>
2009-01-13 20:20:00 +0100

    gui fix: display only non-blank tasks

--- app/controllers/javascripts_controller.rb
+++ app/controllers/javascripts_controller.rb
@@ -3,6 +3,7 @@ class JavascriptsController < ApplicationController
   def dynamic_tasks
     @tasks_by_project = Hash.new
     Task.all.each do |t|
+      next if t.name.blank?
       @tasks_by_project[t.project_id] ||= []
       @tasks_by_project[t.project_id] << { :id => t.id, :name => t.name }
     end
