cc9a92096a2503157a29d94f105210a44f6bfba5
seb <seb@localhost.localdomain>
2009-01-20 20:25:14 +0100

    style improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,23 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+  ICONS = {
+    :edit => ['/images/tango/16x16/apps/accessories-text-editor.png', '16x16'],
+    :destroy => ['/images/tango/16x16/actions/edit-delete.png', '16x16']
+  }
+
   def activeWhenController(name)
     style_class = ''
     if name == @controller.controller_name
       style_class = 'active'
     end
     style_class
+  end
+
+  def icon(type)
+    image_tag ICONS[type][0],
+              :size => ICONS[type][1], :alt => type.to_s.titleize,
+              :title => type.to_s.titleize
   end
 
   def javascript(*files)
