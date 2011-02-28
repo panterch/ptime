2a55c3a6969da3b790f2b6a38ee88d8340467507
preek <alain.lafon@dispatched.ch>
2011-02-28 14:50:50 +0100

    Make start and end of project required. CLoses #706.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
   has_many :tasks
   accepts_nested_attributes_for :tasks
 
-  validates_presence_of :name, :description
+  validates_presence_of :name, :description, :start, :end
   attr_accessible :name, :description, :start, :end, :inactive, :task_ids,
                   :tasks_attributes
 end
