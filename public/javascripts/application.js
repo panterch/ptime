3bd4ba4c60a18265890adb3749bf1462671b31a4
preek <alain.lafon@dispatched.ch>
2011-03-01 15:36:24 +0100

    Remove tasks asynchronously with jQuery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,4 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function remove_inputs(link) {
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest('.inputs').hide();
+}
