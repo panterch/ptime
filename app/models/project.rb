66f255028286fd882b70c6b8a39cb2647f767a75
preek <alain.lafon@dispatched.ch>
2011-03-01 15:39:31 +0100

    Allow destroying of tasks via nested_attributes. Also don't save empty tasks.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   has_many :tasks, :dependent => :destroy
-  accepts_nested_attributes_for :tasks
+  accepts_nested_attributes_for :tasks, :allow_destroy => true, :reject_if => lambda { |task| task[:name].blank? }
 
   validates_presence_of :name, :description, :start, :end
   attr_accessible :name, :description, :start, :end, :inactive, :task_ids,
