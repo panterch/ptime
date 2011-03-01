e2e087c4156802b441a8348375f6bf3eae432624
preek <alain.lafon@dispatched.ch>
2011-03-01 13:50:19 +0100

    Delete associated tasks when a project is deleted

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  has_many :tasks
+  has_many :tasks, :dependent => :destroy
   accepts_nested_attributes_for :tasks
 
   validates_presence_of :name, :description, :start, :end
