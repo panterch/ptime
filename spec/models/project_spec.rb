ea2e2cc456997850bdc6be6dda24a99be7156b26
preek <alain.lafon@dispatched.ch>
2011-03-02 13:34:53 +0100

    Add test for commit e2e087c4156802b441a8348375f6bf3eae432624 (Delete associated tasks when a project is deleted)

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -27,14 +27,30 @@ describe Project do
     assert_equal 1, Project.count
   end
 
-  it "should create a task via mass assignement" do
-    @project = Project.new(:name => "First project",
-               :description => "First description", 
-               :start => Time.now,
-               :end => Time.now+2.days,
-               :tasks_attributes => [{ :name => "First task",
-                                       :inactive => false}])
-    @project.tasks.should_not be_empty
+  context "Mass assignment" do
+    before(:each) do
+      @project = Project.new(:name => "First project",
+                 :description => "First description", 
+                 :start => Time.now,
+                 :end => Time.now+2.days,
+                 :tasks_attributes => [{ :name => "First task",
+                                         :inactive => false}])
+    end
+
+    it "should create a task via mass assignement" do
+      @project.tasks.should_not be_empty
+    end
+
+    it "should delete associated tasks when a parent project is deleted" do
+      @project.save
+      task_id = @project.tasks[0].id
+      @project.destroy
+      begin
+        Task.find(task_id)
+      rescue ActiveRecord::RecordNotFound => e
+        e.message.should match /Couldn't find Task with ID.*/
+      end
+    end
   end
 
 end
