75b54a5682b0c95088585bda2aa397c8f416230e
preek <alain.lafon@dispatched.ch>
2011-03-02 11:02:55 +0100

    Add multiple tasks to a project. Don't show 'delete task' link while editing a project anymore.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,10 @@
-function remove_inputs(link) {
+function remove_fields(link) {
   $(link).prev("input[type=hidden]").val("1");
   $(link).closest('.inputs').hide();
 }
+
+function add_fields(link, association, content) {  
+    var new_id = new Date().getTime();  
+    var regexp = new RegExp("new_" + association, "g");  
+    $(link).before(content.replace(regexp, new_id));  
+}  
