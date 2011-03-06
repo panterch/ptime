202af2a7521f5f05b8d08f4a0fd0aeda6e49cb0c
preek <alain.lafon@dispatched.ch>
2011-03-06 13:29:46 +0100

    Include :inactive input when creating a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,2 @@
 class ProjectsController < InheritedResources::Base
-  # Newly created projects are not inactive
-  def create
-    project = Project.new(params[:project])
-    project.inactive = false
-    project.save
-    redirect_to projects_path
-  end
 end
