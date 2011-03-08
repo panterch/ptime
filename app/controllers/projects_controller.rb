28ea44cd221c5adb90083aa2a1c5e9489d944fb2
preek <alain.lafon@dispatched.ch>
2011-03-08 20:01:52 +0100

    Add missing file for commit:99d438fdabfac9b2c169eb4b729599b9f2e46290

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,11 @@
 class ProjectsController < InheritedResources::Base
+  def new
+    @project = Project.new
+    @project_states = ProjectState.all
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+    @project_states = ProjectState.all
+  end
 end
