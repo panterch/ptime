f420462a2026a9705641f8424f380bd92ad87f9e
preek <alain.lafon@dispatched.ch>
2011-03-08 20:34:01 +0100

    Refactor calculating logic to controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,11 @@ class ProjectsController < InheritedResources::Base
     @project_states = ProjectState.all
   end
 
+  def show
+    @project = Project.find(params[:id])
+    @number_of_inactive_tasks = @project.tasks.where(:inactive => true).size 
+  end
+
   def edit
     @project = Project.find(params[:id])
     @project_states = ProjectState.all
