3ccad0551ece4beaf9130fd6135ba856b8afea11
preek <alain.lafon@dispatched.ch>
2011-03-15 16:40:05 +0100

    Implement show with inherited resources

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,8 +2,9 @@ class ProjectsController < InheritedResources::Base
   before_filter :fetch_project_states, :only => [:new, :edit]
 
   def show
-    @project = Project.find(params[:id])
-    @number_of_inactive_tasks = @project.tasks.where(:inactive => true).size 
+    show! {
+      @number_of_inactive_tasks = @project.tasks.where(:inactive => true).size 
+    }
   end
 
   def fetch_project_states
