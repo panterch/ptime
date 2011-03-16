efd3d697ba84e97d380fc280ed77c5a8b4527a75
preek <alain.lafon@dispatched.ch>
2011-03-16 20:07:48 +0100

    Build user select for entries report form

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,2 +1,9 @@
 module EntriesHelper
+
+  # Build user select for entries report form
+  def users_id_equals_select(form)
+    users = @users.collect { |user| [user.username, user.id] }
+    form.select :user_id_equals, users,  {:include_blank => ''}
+  end
+
 end
