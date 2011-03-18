fa4a28d27fa09e2a5c47cbb7e04252b66810f321
preek <alain.lafon@dispatched.ch>
2011-03-18 13:27:49 +0100

    Add scenario/user story to 'export entries as csv' request spec

--- spec/requests/entries_spec.rb
+++ spec/requests/entries_spec.rb
@@ -1,12 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 
-describe "Entries CSV export" do
-  describe "GET /entries.csv" do
-    it "works! (now write some real specs)" do
-      entry = Factory(:entry)
-      log_in
-      visit "/entries.csv"
-      page.should have_content(entry.day.to_s)
-    end
+feature "Authentication via devise", %q{
+  In order to review the time employees spent on projects
+  When I am logged in
+  I want to have access to a csv report
+} do
+
+  scenario "access csv export" do
+    entry = Factory(:entry)
+    log_in
+    visit "/entries.csv"
+    page.should have_content(entry.day.to_s)
   end
 end
