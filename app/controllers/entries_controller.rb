d7fb2c5bb7a124f374785314944cdb0b241f92e7
preek <alain.lafon@dispatched.ch>
2011-03-23 14:12:15 +0100

    Refactoring  * Rewrite EntriesController without Inherited Resource  * Minimal validation of duration_hours in entry model  * Rewrite showing of flash alerts

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,44 +1,59 @@
-class EntriesController < InheritedResources::Base
+class EntriesController < ApplicationController
   before_filter :get_active_projects, :only => [:new, :edit, :index]
   before_filter :get_tasks_by_project, :only => [:new, :edit, :index]
   before_filter :get_entries, :only => [:new, :edit]
   before_filter :set_day, :only => [:create, :update]
 
 
-  # FIXME: This code has to move. With an implementation like this there will
-  # be crashes on invalid entries.
-  # Problems:
-  #  - Can't move code to model, because current_user should only be read in
-  #    the controller and is inaccessible in the model.
-  #  - Can't move it into a before_filter, because 'resource' can't be assigned
-  #    anything before 'create' itself has run: "Couldn't find Entry without an
-  #    ID"
   def create
-    create! do
-      @entry.user = current_user
-      new_entry_path
-    end
+    @entry = Entry.new(params[:entry])
+    @entry.user = current_user
+    if @entry.save
+      redirect_to new_entry_path, :notice => 'Entry was created.'
+    else
+      redirect_to new_entry_path, :alert => @entry.errors
+     end
   end
 
   # Call from jQuery with the selected date. Get all entries related to that
   # date from the current user.
   def new
-    new! do
-      # When having created or updaten an entry, flash[:day] will be set.
-      # When having clicked the calender widget params[:day] will be set.
-      # Otherwise the user just wants to enter an entry for today.
-      # TODO: Is setting the flash good practice? See set_day()
-      day = params[:day] ? Date.parse(params[:day]) : flash[:day] || Date.today
-      if day
-        @entry = Entry.new
-        @entry.day = day
-        @entries = Entry.find_all_by_user_id_and_day(current_user.id, @entry.day)
-      end
-    end
+    @entry = Entry.new
+    # When having created or updaten an entry, flash[:day] will be set.
+    # When having clicked the calender widget params[:day] will be set.
+    # Otherwise the user just wants to enter an entry for today.
+    # TODO: Is setting the flash good practice? See set_day()
+    day = params[:day] ? Date.parse(params[:day]) : flash[:day] || Date.today
+    @entry.day = day
+    @entries = Entry.find_all_by_user_id_and_day(current_user.id, @entry.day)
   end
 
   def update
-    update! { new_entry_path }
+    @entry = Entry.find(params[:id])
+    if @entry.update_attributes(params[:entry])
+       redirect_to new_entry_path 
+    else
+       @subjects = Subject.find(:all)
+       render :action => 'edit'
+    end
+  end
+
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
+  def edit
+    @entry = Entry.find(params[:id])
+  end
+
+  def delete
+    Entry.find(params[:id]).destroy
+    flash[:notice] = "Successfully destroyed entry."
+    redirect_to new_entry_path
+  end
+
+  def index
+    redirect_to :controller => 'report', :action => 'index'
   end
 
 
