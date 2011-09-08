411114866da39c693c945a605ca4d5643e9ea9e6
Falko Schmidt <scf@panter.ch>
2011-09-08 16:02:40 +0000

    need project_id for mass assignment for now

--- app/models/accounting.rb
+++ app/models/accounting.rb
@@ -2,5 +2,5 @@ class Accounting < ActiveRecord::Base
   belongs_to :project
 
   validates_presence_of :description, :amount, :valuta, :project
-  attr_accessible :description, :amount, :valuta
+  attr_accessible :description, :amount, :valuta, :project_id
 end
