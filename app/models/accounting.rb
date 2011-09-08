4ea6c4f20e4eb01f67fb35fcc979ed1d66e43397
Falko Schmidt <scf@panter.ch>
2011-09-08 14:32:05 +0000

    fix model attributes and validation

--- app/models/accounting.rb
+++ app/models/accounting.rb
@@ -1,6 +1,6 @@
 class Accounting < ActiveRecord::Base
   belongs_to :project
 
-  validates_presence_of :description, :amount, :valuta, :project_id
-  attr_accessible :description, :amount, :valuta, :project_id
+  validates_presence_of :description, :amount, :valuta, :project
+  attr_accessible :description, :amount, :valuta
 end
