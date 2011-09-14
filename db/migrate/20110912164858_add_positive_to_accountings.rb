93ad23cfa5fc85aabaaad6e3bad38328ea6da818
Falko Schmidt <scf@panter.ch>
2011-09-14 13:08:39 +0000

    fix past migration

--- db/migrate/20110912164858_add_positive_to_accountings.rb
+++ db/migrate/20110912164858_add_positive_to_accountings.rb
@@ -1,6 +1,11 @@
 class AddPositiveToAccountings < ActiveRecord::Migration
   def self.up
     add_column :accountings, :positive, :boolean, :null => false
+
+    Accountings.all.each do |accounting|
+      accounting.positive = accounting.amount >= 0 ? true : false
+      accounting.save!
+    end
   end
 
   def self.down
