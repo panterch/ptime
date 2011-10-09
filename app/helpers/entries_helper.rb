2f4cd2c3f30d20ba8811da352c9a7e63f2c9e817
Falko Schmidt <scf@panter.ch>
2011-10-09 18:11:11 +0000

    group projects by recently used and all

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -24,8 +24,28 @@ module EntriesHelper
   # Extract information a form select method
   def to_form_select(projects)
     projects.collect do |p|
-      [truncate([p.shortname, p.description].join(" - "), :length => 26), p.id]
+      [project_identifier(p), p.id]
     end
+  end
+
+  # Extract projects grouped by past usage
+  def grouped_project_select(projects, current_user)
+    recent_projects =
+      User.find(current_user).entries. \
+      order('created_at DESC').limit(5).collect \
+      { |e| [project_identifier(e.project), e.project.id] }
+    group = [['recent projects',recent_projects],
+      ['all projects',to_form_select(projects)]]
+    unless recent_projects.empty?
+      return grouped_options_for_select(group)
+    else
+      return to_form_select(projects)
+    end
+  end
+
+  # Truncated project identifier (shortname & part of the description)
+  def project_identifier(p, length=26)
+    truncate([p.shortname, p.description].join(" - "), :length => length)
   end
 
   # Display an edit link if the entry belongs to the logged in user
