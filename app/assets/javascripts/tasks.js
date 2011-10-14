0b5cae549712e4537bf9a3d0b38ef6919a85574e
Peco Danajlovski <dan@panter.ch>
2011-10-14 16:37:09 +0200

    cleaned up unused debug code

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -2,10 +2,8 @@
 $(function() {
   var taskSelect = '#entry_task_id';
   if ($(taskSelect).length > 0) {
-    console.log('Task selector present!');
     $(taskSelect).live('change', function() {
       var taskID = $(taskSelect + ' option:selected').val();
-      console.log('Selected task ID: ' + taskID);
 
       $.ajax({ url: '/tasks/' + taskID,
                dataType: 'script' })
