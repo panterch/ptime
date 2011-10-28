cdf1a6c403c4ce1cafd19ea16768b0596d5fc755
Peco Danajlovski <dan@panter.ch>
2011-10-28 16:28:33 +0200

    mark specific task as active by default

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,14 @@
 class Task < ActiveRecord::Base
 
+  ACTIVE_BY_DEFAULT = ["Project Management"]
+
   has_paper_trail # versioning
 
   belongs_to :project, :autosave => true
 
   attr_accessible :name, :estimate, :active, :project_id, :billable_by_default
+
+  after_initialize :mark_active
 
   # :active scope with optional boolean argument
   scope :active, lambda {
@@ -24,4 +28,13 @@ class Task < ActiveRecord::Base
   end
 
   alias_method_chain :destroy, :mark
+
+  private
+
+  # Marks active by default all tasks with names contained in the constant ACTIVE_BY_DEFAULT
+  def mark_active
+    if ACTIVE_BY_DEFAULT.include? self.name
+      self.active = true
+    end
+  end
 end
